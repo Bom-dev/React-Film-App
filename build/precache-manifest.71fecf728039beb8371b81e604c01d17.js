835fd0cf99c2a36d3695ccb26948d0ed66250434
Bom <11377ny@gmail.com>
2022-05-30 14:50:30 -0400

    Run build

---
+++ build/precache-manifest.71fecf728039beb8371b81e604c01d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ac0952702cfb3fbe23d8f3d9d46e67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31632ad743842954dfda",
+    "url": "/static/css/main.4f8b8e8d.chunk.css"
+  },
+  {
+    "revision": "d5a8cb7d957f7b052aa8",
+    "url": "/static/js/2.bb28802c.chunk.js"
+  },
+  {
+    "revision": "31632ad743842954dfda",
+    "url": "/static/js/main.797ffbe2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
